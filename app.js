a45e4caabfd41e37d39c2e2ea1e9787955a7c6c1
Alextonkov2004 <alextonkov@gmail.com>
2024-07-06 13:32:00 +0300

    Updating cookies

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const datesRouter = require('./routes/datesRoutes');
 
 //Start express app
 const app = express();
+app.enable('trust proxy')
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
--- controllers/authController.js
+++ controllers/authController.js
@@ -10,17 +10,16 @@ const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
-  };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwt', token, cookieOptions);
+    secure: req.secure || req.headers('x-forward-proto' === 'https')
+  });
   //Remove password from output
   user.password = undefined;
   res.status(statusCode).json({
@@ -44,7 +43,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get('host')}/me`;
   await new Email(newUser, url).sendWelcome();
 
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -56,7 +55,7 @@ exports.login = catchAsync(async (req, res, next) => {
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password', 401));
   }
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 //Only for rendered pages, no errors
 exports.isLoggedIn = catchAsync(async (req, res, next) => {
@@ -197,7 +196,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   //3) Update the ChangedPasswordAt property for the user
 
   //4) Log the user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -215,5 +214,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   user.passwordConfirm = req.body.passwordConfirm;
   await user.save();
   //4) Log user in, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
