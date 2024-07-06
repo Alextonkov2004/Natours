0c533670d54fae5bcee5f8fd46cb814a57aeb62d
Alextonkov2004 <alextonkov@gmail.com>
2024-07-06 13:53:20 +0300

    Fixing cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,14 +12,14 @@ const signToken = (id) =>
   });
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-
-  res.cookie('jwt', token, {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
-    ),
-    httpOnly: true,
-    secure: req.secure || req.headers('x-forward-proto' === 'https')
-  });
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
+  ),
+  httpOnly: true,
+}
+if (process.env.NODE_ENV ==='production') cookieOptions.secure = req.secure || req.headers('x-forward-proto' === 'https')
+  res.cookie('jwt', token, cookieOptions)
   //Remove password from output
   user.password = undefined;
   res.status(statusCode).json({
