0e62c8261f69d926d897ed5ae966652fb6a9d562
Alextonkov2004 <alextonkov@gmail.com>
2024-07-10 18:54:54 +0300

    Fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,7 +25,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     payment_method_types: ['card'],
     mode: 'payment',
     //success_url: `${req.protocol}://${req.gesuccess_url: `${req.protocol}://${req.get('host')}/t('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}&date=${tour.startDates[0]._id}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -96,7 +96,7 @@ exports.checksoldOut = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.bookingCheckout = (req, res, next) => {
+exports.bookingCheckout = (req, res) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
