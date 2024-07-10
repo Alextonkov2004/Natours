06aa50bb4b0f8bc589246ca50f7b70140737fa52
Alextonkov2004 <alextonkov@gmail.com>
2024-07-11 02:31:41 +0300

    Depression

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -75,7 +75,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
-  const price = session.line_items[0].amount_total/100
+  const price = session.amount_total/100
 const date = session.custom_fields[0].dropdown.value
 console.log(tour, user, price, date)
   await Booking.create({tour, user, price, date})
