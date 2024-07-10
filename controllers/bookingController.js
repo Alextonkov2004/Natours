3b95c561be26649ab1f2a32c3e4a93ad708432cb
Alextonkov2004 <alextonkov@gmail.com>
2024-07-10 18:26:35 +0300

    Fixixng

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -76,7 +76,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100
-const date = session.custom_fields[0].dropdown.value
+const date = session.custom_fields.find(field => field.key === 'alex_the_best').dropdown.value
 
   await Booking.create({tour, user, price, date})
 };
