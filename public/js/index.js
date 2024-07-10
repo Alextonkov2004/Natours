6c7db1809edf66fb4c24f971e13165396d82eee4
Alextonkov2004 <alextonkov@gmail.com>
2024-07-11 02:10:14 +0300

    g

--- public/js/index.js
+++ public/js/index.js
@@ -84,5 +84,5 @@ if (bookBtn) {
   });
 }
 
-const alertMessage = document.querySeelector('body').dataset.alert
+const alertMessage = document.querySelector('body').dataset.alert
 if(alertMessage) showAlert('success', alertMessage, 20)
